0b05879fc58d854eb8863aaf22df133350ea2f00
Alexander Aspman <aspmanalexander@gmail.com>
2024-06-26 15:02:14 +0200

    fixed://variables

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,33 +1,36 @@
 import prisma from "../db";
 import { comparePassword, createJWT, hashPassword } from "../modules/auth";
 
-export const createNewUser = async (req,res) =>{
-const user = await prisma.user.create({
-    data:{
-        email:req.body.email,
-        password: await hashPassword(req.body.password),
-       
+export const createNewUser = async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      email: req.body.email,
+      password: await hashPassword(req.body.password),
     }
-})
+  });
 
- const token = createJWT(user)
-res.json({token})
-}
-export const signin = async (req,res) =>{
-    const user = await prisma.user.findUnique({
-        where:{
-            email: req.body.email
-        }
-    })
- const isValid = await comparePassword(req.body.password,user.password)
-if(!isValid){
+  const token = createJWT(user);
+  res.json({ token });
+};
 
-res.status(401)
-res.json({message:'no auth for you! I am not a nazi, there is a valid way to do this.'})
-return 
-}
+export const signin = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: req.body.email
+    }
+  });
 
-const token = createJWT(user)
-res.json({token})
+  if (!user) {
+    res.status(401).json({ message: 'No user found with this email.' });
+    return;
+  }
 
-}
+  const isValid = await comparePassword(req.body.password, user.password);
+  if (!isValid) {
+    res.status(401).json({ message: 'Invalid credentials.' });
+    return;
+  }
+
+  const token = createJWT(user);
+  res.json({ token });
+};
